957153ea8a069af2196693f2369ae7f64446eff0
jphulse <hulsej281@gmail.com>
2026-04-16 12:20:09 -0400

    Made buttons a tool with setter for button overriding

--- infection_spreading/clicker/clicker_passive/clicker_passive_button.gd
+++ infection_spreading/clicker/clicker_passive/clicker_passive_button.gd
@@ -1,1 +1,10 @@
+@tool
 extends Button
+
+@export var texture_icon : TextureRect
+@export var sprite_texture : Texture2D :
+	set(val):
+		sprite_texture = val
+		if texture_icon != null:
+			texture_icon.texture = sprite_texture
+		
